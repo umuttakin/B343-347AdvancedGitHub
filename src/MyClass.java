381b027b1da1b780bfd5a8f850b096f3d61f176e
Umut <umuttakn@gmail.com>
2025-06-24 12:11:12 +0300

    t1

--- src/MyClass.java
+++ src/MyClass.java
@@ -6,6 +6,9 @@ public class MyClass {
     //Version v2
     public String string2 = "Bu da baska bir string'dir.";
 
-    //Version3
+    //Version v3
     public String string3 = "Bu da cok daha baska bir string'dir.";
+
+    //Techpro branch, version t1
+    public String string4 = "Bu techpro branc'inde olusturulmus bir string'dir.";
 }
