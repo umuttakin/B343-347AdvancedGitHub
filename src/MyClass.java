19aba62324b2a788d7c990bf120bece73c7e477b
Umut <umuttakn@gmail.com>
2025-06-24 11:59:43 +0300

    V3

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,8 +1,11 @@
 public class MyClass {
 
     //Version v1
-    public String string1 = "Bu bir string' dir.";
+    public String string1 = "Bu bir string'dir.";
 
     //Version v2
-    public String string2 = "Bu da baska bir string' dir";
+    public String string2 = "Bu da baska bir string'dir.";
+
+    //Version3
+    public String string3 = "Bu da cok daha baska bir string'dir.";
 }
